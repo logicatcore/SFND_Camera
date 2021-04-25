ae19987f151c00a91e979d17ef567fa8a1b53a10
sai <kakubalsai@gmail.com>
2021-04-25 12:25:07 +0200

    Gaussian smoothing exercise

--- Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
+++ Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/gaussian_smoothing.cpp
@@ -13,13 +13,17 @@ void gaussianSmoothing1()
     img = cv::imread("../images/img1gray.png");
 
     // create filter kernel
-    float gauss_data[25] = {1, 4, 7, 4, 1,
+    float gauss_data[25] = {1,  4,  7,  4, 1,
                             4, 16, 26, 16, 4,
                             7, 26, 41, 26, 7,
                             4, 16, 26, 16, 4,
-                            1, 4, 7, 4, 1};
+                            1,  4,  7,  4, 1};
     cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
-
+    
+    // normalise the kernel
+    int total = cv::sum(kernel)[0];
+    kernel /= total;
+    
     // apply filter
     cv::Mat result;
     cv::filter2D(img, result, -1, kernel, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
