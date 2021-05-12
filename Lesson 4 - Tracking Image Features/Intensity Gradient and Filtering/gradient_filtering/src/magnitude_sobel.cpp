4aad3598d1f9f31100c9cfcb13451090ff4a9a3c
Sai <kakubalsai@gmail.com>
2021-05-12 23:16:47 +0200

    Sobel exercise solution

--- Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/magnitude_sobel.cpp
+++ Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/magnitude_sobel.cpp
@@ -10,7 +10,7 @@ void magnitudeSobel()
 {
     // load image from file
     cv::Mat img;
-    img = cv::imread("./images/img1gray.png");
+    img = cv::imread("../images/img1gray.png");
 
     // convert image to grayscale
     cv::Mat imgGray;
@@ -18,17 +18,35 @@ void magnitudeSobel()
 
     // apply smoothing using the GaussianBlur() function from the OpenCV
     // ToDo : Add your code here
+    cv::Mat blurredImage;// = imgGray.clone(); NOT NECESSARY
+    cv::Size kernelSize(5, 5);
+    float stdDev(2.0);
+    cv::GaussianBlur(imgGray, blurredImage, kernelSize, stdDev);
 
     // create filter kernels using the cv::Mat datatype both for x and y
     // ToDo : Add your code here
+    float entriesX[9] = {-1, 0, +1, -2, 0, +2, -1, 0, +1};
+    cv::Mat sobelX(3, 3, CV_32F, entriesX);
+
+    float entriesY[9] = {-1, -2, -1, 0, 0, 0, +1, +2, +1};
+    cv::Mat sobelY(3, 3, CV_32F, entriesY); 
 
     // apply filter using the OpenCv function filter2D()
     // ToDo : Add your code here
-    
+    cv::Mat gradientX, gradientY;
+
+    cv::filter2D(blurredImage, gradientX, -1, sobelX);//, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+    cv::filter2D(blurredImage, gradientY, -1, sobelY);//, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
 
     // compute magnitude image based on the equation presented in the lesson 
     // ToDo : Add your code here
-    
+    cv::Mat magnitude = imgGray.clone();
+
+    for(int i = 0; i < magnitude.rows; i++){
+        for(int j = 0; j < magnitude.cols; j++){
+            magnitude.at<unsigned char>(i,j) = sqrt(pow(gradientX.at<unsigned char>(i,j), 2) + pow(gradientY.at<unsigned char>(i,j), 2));
+        }
+    }
 
     // show result
     string windowName = "Gaussian Blurring";
